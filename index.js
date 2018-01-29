7d4c6117ee9bf7be4e80da2b34243627f5f6168d
Peter Babeu <mbeenpete@hotmail.com>
2018-01-29 14:28:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var newArray = [];
   var i;
-  for (i=0; i < musicians[musicians.length]; i++) {
+  for (i=0; i < musicians.length; i++) {
     newArray[i]=musicians[i] + ' plays ' + instruments[i];
   }
   return newArray; 
