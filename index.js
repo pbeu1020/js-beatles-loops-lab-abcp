5296d3ce7a105d6ab6ecd29bc7158f9cdc07cf83
Peter Babeu <mbeenpete@hotmail.com>
2018-01-29 14:26:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function theBeatlesPlay(musicians, instruments) {
   var i;
   for (i=0; i < musicians[musicians.length]; i++) {
     newArray[i]=musicians[i] + ' plays ' + instruments[i];
-     
   }
+  return newArray; 
 }
 
 // 
