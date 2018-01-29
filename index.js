3db5d34237779703b3254c952181fc3b0d28da85
Peter Babeu <mbeenpete@hotmail.com>
2018-01-29 15:24:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+// 
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = [];
+  var i;
+  for (i=0; i < musicians.length; i++) {
+    newArray[i]=musicians[i] + ' plays ' + instruments[i];
+  }
+  return newArray; 
+}
+
+// 
+function johnLennonFacts(facts) {
+  var i=0;
+  while (i < facts.length) {
+    facts[i] = facts[i] + '!!!'; 
+    i++;
+  }
+  return facts;
+}
+
+// 
+function iLoveTheBeatles(n) {
+  var newArray = [];
+  var i = n;
+  do {
+    newArray[n-i] = "I love the Beatles!";
+    if (n === 17) { break; }
+    n++;
+  }
+  while (n < 15);
+  return newArray;
+}
